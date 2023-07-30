041ff2bfad8c6cbf662642cf1fcf5f8ed673ad19
Deepak Kumar <91386273+Deepakbst1@users.noreply.github.com>
2023-07-30 23:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+ document.getElementById("button").addEventListener("click", function(){
+	  const name = document.getElementById("name").value;
+      const year = document.getElementById("year").value;
+
+	  const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+	  const urlElement = document.getElementById("url");
+	 urlElement.textContent = "https://localhost:8080/" + queryString;
+
+ })
